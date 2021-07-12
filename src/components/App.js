d32d84c73fc9123a30edff7e8fd929b2154baaf4
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-07-12 19:18:53 +0300

    added main styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
 import Main from './main/Main';
+import styles from "./App.module.css"
 
 const App = () => {
     return (
-        <>
+        <div className={styles.container}>
             <Header/>
             <Main />
-        </>
+        </div>
     );
 }
 
