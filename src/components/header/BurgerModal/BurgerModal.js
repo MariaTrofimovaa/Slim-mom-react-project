6cdbee8610830d55c71450b200b949f54704434a
Russel <blokhin.rus@gmail.com>
2021-07-14 16:37:40 +0300

    yo

---
+++ src/components/header/BurgerModal/BurgerModal.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { routes } from "../../../routes/routes";
+import style from "./Burger.module.css";
+import cls from "classnames";
+
+import { NavLink, useLocation } from "react-router-dom";
+const BurgerModal = ({ toglBurger, hendleToglBurger, authToken }) => {
+  const location = useLocation();
+
+  return (
+    <>
+      <button
+        className={cls(toglBurger ? style.burgerCloseBtn : style.burgerOpenBtn)}
+        type="button"
+        onClick={hendleToglBurger}
+      ></button>
+
+      <div
+        className={cls(
+          toglBurger
+            ? style.burgerModalWrapperOpen
+            : style.burgerModalWrapperClose
+        )}
+      >
+        <ul className={style.burgerList}>
+          {routes.map((item) => (
+            <li key={item.path} className={style.navItem}>
+              {authToken && !item.restricted && (
+                <NavLink
+                  onClick={hendleToglBurger}
+                  to={{
+                    pathname: item.path,
+                    state: { from: location.pathname },
+                  }}
+                  className={style.navItemLink}
+                  activeClassName={style.activ}
+                  exact={item.exact}
+                >
+                  {item.name}
+                </NavLink>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default BurgerModal;
