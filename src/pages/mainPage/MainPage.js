5f4da497f770236bcd716ed6611e2547d91bd2ae
MaksymZakharenko <raspik83@gmail.com>
2021-07-14 20:34:24 +0300

    Valera

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import DailyCaloriesForm from "../../components/dailyCaloriesForm/DailyCaloriesForm";
 import Modal from "../../components/modal/Modal";
 import DailyKkalIntake from "../../components/kkalInfo/DailyKkalIntake";
+import RightSideBar from "../../components/rightSideBar/RightSideBar";
 
 const BASE_URL = "https://slimmom-backend.goit.global";
 
@@ -24,16 +25,16 @@ const MainPage = () => {
     const data = transformString(values);
     const res = await axios.post(`${BASE_URL}/daily-rate`, data);
     setData(res.data);
-    console.log(res.data);
+
     setShowModal(true);
   };
-  console.log(data);
   return (
     <>
       <DailyCaloriesForm onSubmit={onSubmit} />
       {showModal && (
         <Modal onClick={onToggleModal} onClose={onToggleModal}>
           <DailyKkalIntake {...data} />
+          <RightSideBar {...data} />
         </Modal>
       )}
     </>
