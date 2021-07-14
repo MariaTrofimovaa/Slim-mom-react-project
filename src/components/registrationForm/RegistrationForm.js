f140612a3f98a4a3e7da8366696e8eb8360071a4
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-07-14 16:46:50 +0300

    calendar added

--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { register } from "../../redux/auth/auth.operations";
-import styles from "./RegistrationForm.module.css";
+import schema from "../loginForm/validator/Validator";
+import style from "./RegistrationForm.module.css";
 
 const InitialState = {
   username: "",
@@ -12,103 +14,220 @@ const InitialState = {
 };
 
 const RegistrationForm = () => {
-  const [user, setUser] = useState(InitialState);
+  // const [user, setUser] = useState(InitialState);
   const dispatch = useDispatch();
   const location = useLocation();
+  const history = useHistory();
 
-  const onHandleChange = (event) => {
-    const { name, value } = event.target;
-    setUser((prevState) => ({ ...prevState, [name]: value }));
-  };
+  // const onHandleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setUser((prevState) => ({ ...prevState, [name]: value }));
+  // };
 
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
+  // const onHandleSubmit = (e) => {
+  //   e.preventDefault();
 
-    dispatch(register(user));
-    setUser(InitialState);
-  };
+  //   dispatch(register(user));
+  //   setUser(InitialState);
+  // };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.registerContainer}>
-        <div className={styles.formContainer}>
-          <h1 className={styles.authTitle}>Регистрация</h1>
-          <form onSubmit={onHandleSubmit} className={styles.registerForm}>
-            <label htmlFor="" className={styles.label}>
-              {/* Имя* */}
-              <input
-                id="username"
-                name="username"
+    <div className={style.container}>
+      <div className={style.formContainer}>
+        <h1 className={style.authTitle}>Регистрация</h1>
+        <Formik
+          initialValues={InitialState}
+          validationSchema={schema}
+          onSubmit={(user) => {
+            dispatch(register(user));
+            history.push("/login");
+          }}
+        >
+          <Form className={style.form}>
+            <label>
+              <Field
                 type="text"
-                className={styles.inputData}
-                placeholder="Имя*"
-                value={user.username}
-                onChange={onHandleChange}
-                required
+                name="username"
+                placeholder="Имя *"
+                className={style.input}
               />
-            </label>
-
-            <label htmlFor="email" className={styles.label}>
-              {/* Логин* */}
-              <input
-                id="email"
+              <ErrorMessage
                 name="email"
-                type="mail"
-                className={styles.inputData}
-                placeholder="Логин*"
-                value={user.email}
-                onChange={onHandleChange}
-                required
+                component="div"
+                className={style.errorMessage}
               />
             </label>
 
-            <label htmlFor="password" className={styles.label}>
-              {/* Пароль* */}
-              <input
-                id="password"
-                name="password"
+            <label>
+              <Field
+                type="email"
+                name="email"
+                placeholder="Логин *"
+                className={style.input}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={style.errorMessage}
+              />
+            </label>
+
+            <label>
+              <Field
                 type="password"
-                className={styles.inputData}
-                placeholder="Пароль*"
-                value={user.password}
-                onChange={onHandleChange}
-                required
+                name="password"
+                placeholder="Пароль *"
+                className={style.input}
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={style.errorMessage}
               />
             </label>
-          </form>
-        </div>
+            {/* <button type="submit" className={style.buttons}>
+              Вход
+            </button> */}
+            <div className={style.buttonContainer}>
+              <button className={style.authButton} type="submit">
+                {location.pathname === "/registration" ? "Регистрация" : "Bход"}
+              </button>
 
-        <div className={styles.buttonContainer}>
-          <button className={styles.authButton} type="submit">
-            {location.pathname === "/registration" ? "Регистрация" : "Bход"}
-          </button>
-
-          {location.pathname === "/registration" ? (
-            <Link to="/login" className={styles.authLink}>
-              {location.pathname === "/registration" ? "Вход" : "Регистрация"}
-            </Link>
-          ) : (
-            <Link to="/registration" className={styles.authLink}>
-              {location.pathname === "/registration"
-                ? "Уже есть аккаунт"
-                : "Хочу зарегистрироваться"}
-            </Link>
-          )}
-        </div>
-
-        {/* <div className={styles.btnWrapper}>
-          <button type="submit" className={styles.submitBtn}>
-            Регистрация
-          </button>
-
-          <p className={styles.authText}>У вас уже усть аккаунт?</p>
-          <Link to="/login" className={styles.underlined}>
-            Вход
-          </Link>
-        </div> */}
+              {location.pathname === "/registration" ? (
+                <Link to="/login" className={style.authLink}>
+                  {location.pathname === "/registration"
+                    ? "Вход"
+                    : "Регистрация"}
+                </Link>
+              ) : (
+                <Link to="/registration" className={style.authLink}>
+                  {location.pathname === "/registration"
+                    ? "Уже есть аккаунт"
+                    : "Регистрация"}
+                </Link>
+              )}
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
 };
 
 export default RegistrationForm;
+
+// ========================================
+// import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { Link } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+// import { register } from "../../redux/auth/auth.operations";
+// import styles from "./RegistrationForm.module.css";
+
+// const InitialState = {
+//   username: "",
+//   email: "",
+//   password: "",
+// };
+
+// const RegistrationForm = () => {
+//   const [user, setUser] = useState(InitialState);
+//   const dispatch = useDispatch();
+//   const location = useLocation();
+
+//   const onHandleChange = (event) => {
+//     const { name, value } = event.target;
+//     setUser((prevState) => ({ ...prevState, [name]: value }));
+//   };
+
+//   const onHandleSubmit = (e) => {
+//     e.preventDefault();
+
+//     dispatch(register(user));
+//     setUser(InitialState);
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.registerContainer}>
+//         <div className={styles.formContainer}>
+//           <h1 className={styles.authTitle}>Регистрация</h1>
+//           <form onSubmit={onHandleSubmit} className={styles.registerForm}>
+//             <label htmlFor="" className={styles.label}>
+//               {/* Имя* */}
+//               <input
+//                 id="username"
+//                 name="username"
+//                 type="text"
+//                 className={styles.inputData}
+//                 placeholder="Имя*"
+//                 value={user.username}
+//                 onChange={onHandleChange}
+//                 required
+//               />
+//             </label>
+
+//             <label htmlFor="email" className={styles.label}>
+//               {/* Логин* */}
+//               <input
+//                 id="email"
+//                 name="email"
+//                 type="mail"
+//                 className={styles.inputData}
+//                 placeholder="Логин*"
+//                 value={user.email}
+//                 onChange={onHandleChange}
+//                 required
+//               />
+//             </label>
+
+//             <label htmlFor="password" className={styles.label}>
+//               {/* Пароль* */}
+//               <input
+//                 id="password"
+//                 name="password"
+//                 type="password"
+//                 className={styles.inputData}
+//                 placeholder="Пароль*"
+//                 value={user.password}
+//                 onChange={onHandleChange}
+//                 required
+//               />
+//             </label>
+//           </form>
+//         </div>
+
+//         <div className={styles.buttonContainer}>
+//           <button className={styles.authButton} type="submit">
+//             {location.pathname === "/registration" ? "Регистрация" : "Bход"}
+//           </button>
+
+//           {location.pathname === "/registration" ? (
+//             <Link to="/login" className={styles.authLink}>
+//               {location.pathname === "/registration" ? "Вход" : "Регистрация"}
+//             </Link>
+//           ) : (
+//             <Link to="/registration" className={styles.authLink}>
+//               {location.pathname === "/registration"
+//                 ? "Уже есть аккаунт"
+//                 : "Хочу зарегистрироваться"}
+//             </Link>
+//           )}
+//         </div>
+
+//         {/* <div className={styles.btnWrapper}>
+//           <button type="submit" className={styles.submitBtn}>
+//             Регистрация
+//           </button>
+
+//           <p className={styles.authText}>У вас уже усть аккаунт?</p>
+//           <Link to="/login" className={styles.underlined}>
+//             Вход
+//           </Link>
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RegistrationForm;
--- src/components/registrationForm/validator/Validator.js
+++ src/components/registrationForm/validator/Validator.js
@@ -5,16 +5,17 @@ let schema = yup.object().shape({
     .string()
     .min(3, "*Минимум 3 символа")
     .max(50, "*Слишком длинное имя"),
+  // .required("*Поле обязательно"),
   email: yup
     .string()
     .min(3, "*Минимум 3 символа")
     .max(50, "*Слишком длинный email")
     .required("*Поле обязательно"),
-  // password: yup
-  //   .string()
-  //   .min(7, "*Минимум 7 символов")
-  //   .max(50, "*Слишком длинный пароль")
-  //   .required("*Поле обязательно"),
+  password: yup
+    .string()
+    .min(7, "*Минимум 7 символов")
+    .max(50, "*Слишком длинный пароль")
+    .required("*Поле обязательно"),
 });
 
 export default schema;
